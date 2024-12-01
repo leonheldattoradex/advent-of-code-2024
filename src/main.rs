fc2edde8de0eec3d492314ed9bf4eab67c9fb2cf
Leonardo Held <leonardo.held@toradex.com>
2024-12-01 14:09:12 -0300

    day01: fix stupid counter logic by using iterator
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut input_map = HashMap::<String, String>::new();
 
-    let mut counter = 0;
-
     for line in reader.lines() {
         let line_result = line?;
-        let line_split = line_result.split("   ");
+        let mut iter = line_result.split("   ");
 
-        let mut old_part: String = String::new();
-
-        for part in line_split {
-            counter += 1;
-            if counter % 2 == 0 {
-                input_map.insert(old_part.clone(), part.to_string());
-            } else {
-                old_part = part.to_string();
-            }
+        while let (Some(key), Some(value)) = (iter.next(), iter.next()) {
+            input_map.insert(key.to_string(), value.to_string());
         }
+    }
 
-        println!("{}, {}", old_part, input_map[&old_part]);
+    for (key, value) in input_map {
+        println!("{}, {}", key, value);
     }
 
     Ok(())
