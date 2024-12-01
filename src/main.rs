a772f5eca0843f5394212fd4e748a2d9a918076e
Leonardo Held <leonardo.held@toradex.com>
2024-12-01 14:38:32 -0300

    day01: finish part one
    Sort, get Euclidian distance, sum.
    
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -6,20 +5,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
-    let mut input_map = HashMap::<String, String>::new();
+    let mut a: Vec<i32> = Vec::new();
+    let mut b: Vec<i32> = Vec::new();
 
     for line in reader.lines() {
         let line_result = line?;
         let mut iter = line_result.split("   ");
 
         while let (Some(key), Some(value)) = (iter.next(), iter.next()) {
-            input_map.insert(key.to_string(), value.to_string());
+            a.push(key.to_string().parse::<i32>().unwrap());
+            b.push(value.to_string().parse::<i32>().unwrap());
         }
     }
 
-    for (key, value) in input_map {
-        println!("{}, {}", key, value);
+    a.sort();
+    b.sort();
+
+    let mut c: Vec<i32> = Vec::new();
+    for (key, value) in a.iter().zip(b.iter()) {
+        let r = key - value;
+        c.push(r.abs());
     }
+
+    let sum: i32 = c.iter().sum();
+    println!("{}", sum);
 
     Ok(())
 }
