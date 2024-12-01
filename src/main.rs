3cecfe61cb7143dff61d07ddfea1b67897928725
Leonardo Held <leonardo.held@toradex.com>
2024-12-01 15:01:45 -0300

    day01: compute similary score
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use std::fs::File;
+use std::hash::Hash;
 use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,17 +20,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    a.sort();
-    b.sort();
+    // clone and dedup the left list
+    let mut a_clone = a.clone();
+    a_clone.dedup();
 
-    let mut c: Vec<i32> = Vec::new();
-    for (key, value) in a.iter().zip(b.iter()) {
-        let r = key - value;
-        c.push(r.abs());
+    // iterate against the right list and count how many instances there are
+    // for each deduped element in a
+    let mut frequency_map = HashMap::<i32, i32>::new();
+    let mut counter = 0;
+    for key in a_clone.iter() {
+        for value in b.iter() {
+            if value == key {
+                counter += 1;
+            }
+        }
+        frequency_map.insert(*key, counter);
+        println!("{}", counter);
+        counter = 0;
     }
 
-    let sum: i32 = c.iter().sum();
-    println!("{}", sum);
+    let mut similarity_score: i32 = 0;
+    // frequency_map is now a <element in left list, frequency in right list> map
+    for (key, value) in a.iter().zip(b.iter()) {
+        similarity_score += key * frequency_map.get(key).unwrap();
+    }
+    println!("{}", similarity_score);
 
     Ok(())
 }
